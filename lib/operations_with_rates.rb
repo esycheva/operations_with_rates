cea8affa2183ec91acb3f552ab026a552c7235e3
Elena Istomina <sychevaea@gmail.com>
2023-09-04 15:57:12 +0300

    The beginning of the gem

---
+++ lib/operations_with_rates.rb
@@ -0,0 +1,49 @@
+# Решение задач:
+# Число (система счисления с основанием 10) 3*16**2018 -2*8**1028 -3*4**1100 - 2**1050 -2022
+# записали в системе счисления с основанием 4. Сколько 3-ек содержит эта запись?
+class OperationsWithRates
+  attr_reader :decimal_number, :base, :couting_char
+
+  def initialize(decimal_number, base, couting_char)
+    @decimal_number = decimal_number
+    @base = base
+    @couting_char = couting_char
+    @result = nil
+  end
+
+  def culculate
+    count = 0
+    base_number = ''
+    a = @decimal_number
+    while a > 0 do
+      remainder = a % base
+      a = a / base
+      base_number << remainder.to_s
+    end
+    base_number.reverse!
+    base_number.each_char do |char|
+      if char == couting_char
+        count += 1
+      end
+    end
+    count
+  end
+
+  def show_result
+    puts "RESULT IS #{culculate}"
+  end
+end
+
+# Задача:
+# Число (система счисления с основанием 10) 3*16**2018 -2*8**1028 -3*4**1100 - 2**1050 -2022
+# записали в системе счисления с основанием 4. Сколько 3-ек содержит эта запись?
+decimal_number = 3*16**2018 -2*8**1028 -3*4**1100 - 2**1050 -2022
+base = 4
+couting_char = 3.to_s
+
+operations_with_rates = OperationsWithRates.new(decimal_number, base, couting_char)
+
+puts "DECIMAL_NUMBER  #{operations_with_rates.decimal_number}"
+puts "BASE   #{operations_with_rates.base}"
+puts "COUTING_CHAR  #{operations_with_rates.couting_char}"
+operations_with_rates.show_result
---
+++ lib/operations_with_rates/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module OperationsWithRates
+  VERSION = "0.1.0"
+end
